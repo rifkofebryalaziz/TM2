3fd8da091d9beab2f333aff58f13e702b76c94a3
rifkofebryalaziz <rifkofebryalaziz30@gmail.com>
2023-06-24 15:53:50 +0700

    Bilangan Fibonacci.java

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Fibonacci {
+public class Fibonacci {
+    public static void main(String[] args) {
+        int previousNumber = 0;
+        int currentNumber = 1;
+        int sum = 0;
+        int count = 0;
+
+        System.out.println("Deretan bilangan Fibonacci Genap Dari 10 - 4500 :");
+        for (int i = 0; currentNumber <= 4500; i++) {
+            if (currentNumber >= 10 && currentNumber % 2 == 0) {
+                System.out.println(currentNumber);
+                sum += currentNumber;
+                count++;
+            }
+
+            int nextNumber = previousNumber + currentNumber;
+            previousNumber = currentNumber;
+            currentNumber = nextNumber;
+        }
+
+        System.out.println("Jumlah Deret Bilangan Fibonacci: " + count);
+        System.out.println("Jumlah Total Seluruh Bilangan Genap Fibonacci: " + sum);
+    }
 }
